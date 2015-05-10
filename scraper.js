0371ff9ad96f881a0eaeef40e4ad929265504dcc
Guido Bouman <m@guido.vc>
2015-05-10 14:42:47 +0200

    Move comments to bottom.

--- scraper.js
+++ scraper.js
@@ -16,31 +16,6 @@ request(config.scrapeUrl, function(error, response, html) {
       return
     }
 
-    // 00 : ID            'ReaOP1135624512',
-    // 01 : ?             '1',
-    // 02 : Lat,Lng       '52.3569568,4.79270919999999',
-    // 03 : Postcode      '1069DW',
-    // 04 : Stad          'Amsterdam',
-    // 05 : Wijk          'Amsterdam, Sloten/Slotervaart/Osdorp',
-    // 06 : Straat        'Tussen Meer',
-    // 07 : Huisnummer    '226',
-    // 08 : Toevoeging    '',
-    // 09 : ?             'WH',
-    // 10 : ?             '1',
-    // 11 : Type woning   'Portiekflat',
-    // 12 : Vol addres    'Tussen Meer 226 1069 DW Amsterdam, Sloten/Slotervaart/Osdorp ',
-    // 13 : Prijs HTML    '<b>Koopsom</b><br/>€ 160.000 ',
-    // 14 : Prijs raw     '160000',
-    // 15 : Prijs type    'k.k.',
-    // 16 : Prijs beschr. 'koopsom',
-    // 17 : Prijs nice    '160.000',
-    // 18 : URL           'http://www.mva.nl/huizen/Tussen-Meer-226-1069DW-Amsterdam-ReaOP1135624512.html',
-    // 19 : Thumbnail     'http://media.mva.nl/MVASysteem/WONEN/ReaOP1135624512/1.jpg',
-    // 20 : ?             '0',
-    // 21 : ?             '1',
-    // 22 : Datum         '14/04/2015',
-    // 23 : Status        '1' // 1: te koop, 2: verkocht, 6: onder bod
-
     var data = $(this).attr('value').split('~')
     var object = {
       id: data[0],
@@ -53,3 +28,28 @@ request(config.scrapeUrl, function(error, response, html) {
     console.log(object)
   });
 })
+
+// 00 : ID            'ReaOP1135624512',
+// 01 : ?             '1',
+// 02 : Lat,Lng       '52.3569568,4.79270919999999',
+// 03 : Postcode      '1069DW',
+// 04 : Stad          'Amsterdam',
+// 05 : Wijk          'Amsterdam, Sloten/Slotervaart/Osdorp',
+// 06 : Straat        'Tussen Meer',
+// 07 : Huisnummer    '226',
+// 08 : Toevoeging    '',
+// 09 : ?             'WH',
+// 10 : ?             '1',
+// 11 : Type woning   'Portiekflat',
+// 12 : Vol addres    'Tussen Meer 226 1069 DW Amsterdam, Sloten/Slotervaart/Osdorp ',
+// 13 : Prijs HTML    '<b>Koopsom</b><br/>€ 160.000 ',
+// 14 : Prijs raw     '160000',
+// 15 : Prijs type    'k.k.',
+// 16 : Prijs beschr. 'koopsom',
+// 17 : Prijs nice    '160.000',
+// 18 : URL           'http://www.mva.nl/huizen/Tussen-Meer-226-1069DW-Amsterdam-ReaOP1135624512.html',
+// 19 : Thumbnail     'http://media.mva.nl/MVASysteem/WONEN/ReaOP1135624512/1.jpg',
+// 20 : ?             '0',
+// 21 : ?             '1',
+// 22 : Datum         '14/04/2015',
+// 23 : Status        '1' // 1: te koop, 2: verkocht, 6: onder bod
